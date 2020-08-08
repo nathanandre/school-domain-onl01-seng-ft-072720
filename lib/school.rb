90f2fcbd7cc20fb405c15a7ee0710adfc62e224a
Nathan Burns <nathanandreburns@gmail.com>
2020-08-08 17:14:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  def initialize (name)
+ @name = name
+  @roster = {}
+  end
+    def roster
+      @roster 
+    end
+    def add_student
+  student.new = {}
+  name = "name"
+  puts name
+end
+end
