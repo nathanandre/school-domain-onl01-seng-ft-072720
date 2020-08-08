0f46728185eba3605f50af8c007f36bbc4dfd308
Nathan Burns <nathanandreburns@gmail.com>
2020-08-08 17:15:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,12 @@
+require 'pry'
 class School
-  def initialize (name)
- @name = name
-  @roster = {}
+  attr_accessor :name, :roster
+  def initialize(name) 
+    @name = name 
+    @roster = {}
   end
-    def roster
-      @roster 
-    end
-    def add_student
-  student.new = {}
-  name = "name"
-  puts name
-end
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
 end
